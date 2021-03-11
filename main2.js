f5b7b2575e4e88b43db96405fd2f188cad6b19e6
Ayyoub Kamli <akamli@e1r4p8.1337.ma>
2021-03-11 18:43:49 +0100

    quadraticEquation is solved in the Discriminant is positive

--- main2.js
+++ main2.js
@@ -1,5 +1,4 @@
-const eq =
-  "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0- 8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 ";
+const eq = "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0";
 
 const splitByEqual = async (equation) => {
   return await equation.replace(/ /g, "").replace(/-/g, "+-").split("=");
@@ -36,7 +35,141 @@ const le = (e) => {
   return getNumberaExp(array, e.sign);
 };
 
-const main = async () => {
+const symplifyElement = (e) => {
+  let n = { number: null, exp: null };
+
+  n.number = e.number * e.sign;
+  n.exp = e.exp;
+  return n;
+};
+
+const symplify = (e) => {
+  let result = [];
+  let n = {};
+  e.map((el) => {
+    n = symplifyElement(el);
+    result.push(n);
+  });
+  return result;
+};
+
+const reduce = (e) => {
+  let i = 0;
+  let j = 0;
+
+  while (i < e.length) {
+    j = i + 1;
+    while (j < e.length) {
+      if (e[i].exp == e[j].exp) {
+        e[i].number = e[i].number + e[j].number;
+        e.splice(j, 1);
+
+        j -= 1;
+      }
+      j++;
+    }
+    i++;
+  }
+
+  return e;
+};
+
+const sort = (e) => {
+  let i = 0;
+  let j = 0;
+  let n = {};
+  while (i < e.length) {
+    j = i + 1;
+    while (j < e.length) {
+      if (e[i].exp > e[j].exp) {
+        n = e[i];
+        e[i] = e[j];
+        e[j] = n;
+      }
+      j++;
+    }
+    i++;
+  }
+  return e;
+};
+
+const rewrit = (e) => {
+  let eq = "";
+  let i = 0;
+  let j = 0;
+
+  while (i < e.length) {
+    j = i + 1;
+    if (e[i].number < 0 && j < e.length) {
+      eq = eq.concat(" - ");
+      eq = eq.concat(`${e[i].number * -1} * X^${e[i].exp}`);
+    } else {
+      if (i > 0) {
+        eq = eq.concat(" + ");
+      }
+      eq = eq.concat(`${e[i].number} * X^${e[i].exp}`);
+    }
+    i++;
+  }
+
+  eq = eq.concat(" = 0");
+  console.log(eq);
+};
+
+const checkDegree = (e) => {
+  let d = e[0].exp;
+  e.map((el) => {
+    if (el.exp > d) {
+      d = el.exp;
+    }
+  });
+  return d;
+};
+
+const quadraticEquation = (e) => {
+  let a = 0;
+  let b = 0;
+  let c = 0;
+  // console.log(e);
+  e.map((el) => {
+    if (el.exp == 2) {
+      a = el.number;
+    }
+
+    if (el.exp == 1) {
+      b = el.number;
+    }
+
+    if (el.exp == 0) {
+      c = el.number;
+    }
+  });
+  console.log(`Coefficients are: a = ${a} , b = ${b} , c = ${c} `);
+  console.log(`Discriminant = b^2 − 4ac`);
+  console.log("Quadratic Formula: X =  (−b ± √|Discriminant|) / 2a");
+  let d = b * b - 4 * a * c;
+  if (d > 0) {
+    console.log(
+      `the Discriminant is Positive: b^2 − 4ac = ${b}^2 - 4 * ${a} * ${c}`
+    );
+    console.log(`Discriminant = ${d}`);
+    console.log(`Put in a, b and c:	X = (${b * -1} ± √|${d}|) / 2 * ${a}`);
+    d = Math.sqrt(d);
+    console.log(`	X = (${b * -1} ± ${d}) /  ${a * 2}`);
+    console.log(
+      `	X = (${b * -1 - d}) /  ${a * 2} or X = (${b * -1 + d}) /  ${a * 2}`
+    );
+    console.log(
+      `	X = ${(b * -1 - d) / (a * 2)} or X = ${(b * -1 + d) / (a * 2)}`
+    );
+  }
+};
+
+const linearEquation = (e) => {
+  console.log("linearEquation");
+};
+
+const main = () => {
   try {
     splitByEqual(eq)
       .then((result) => {
@@ -45,7 +178,33 @@ const main = async () => {
         return left.concat(right);
       })
       .then((result) => {
-        console.log(result);
+        return symplify(result);
+      })
+      .then((result) => {
+        return reduce(result);
+      })
+      .then((result) => {
+        return sort(result);
+      })
+      .then((result) => {
+        rewrit(result);
+        return result;
+      })
+      .then((result) => {
+        // console.log(result);
+        const degree = checkDegree(result);
+        console.log(`Polynomial degree: ${degree}`);
+        if (degree > 2) {
+          console.log(
+            "The polynomial degree is strictly greater than 2, I can't solve."
+          );
+        } else if (degree == 2) {
+          quadraticEquation(result);
+        } else if (degree == 1) {
+          linearEquation(result);
+        } else {
+          console.log("else");
+        }
       });
   } catch (err) {
     console.log(err);
