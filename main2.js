3d7e678b621af00affb1c0a398e8dd34932ad561
Ayyoub Kamli <akamli@e1r4p8.1337.ma>
2021-03-11 19:08:10 +0100

    I add the solution of discriminant is negative

--- main2.js
+++ main2.js
@@ -1,4 +1,4 @@
-const eq = "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0";
+const eq = "1 * X^0 + 2 * X^1 + 5 * X^2 = 0";
 
 const splitByEqual = async (equation) => {
   return await equation.replace(/ /g, "").replace(/-/g, "+-").split("=");
@@ -150,7 +150,7 @@ const quadraticEquation = (e) => {
   let d = b * b - 4 * a * c;
   if (d > 0) {
     console.log(
-      `the Discriminant is Positive: b^2 − 4ac = ${b}^2 - 4 * ${a} * ${c}`
+      `The Discriminant is Positive: b^2 − 4ac = ${b}^2 - 4 * ${a} * ${c}`
     );
     console.log(`Discriminant = ${d}`);
     console.log(`Put in a, b and c:	X = (${b * -1} ± √|${d}|) / 2 * ${a}`);
@@ -161,6 +161,17 @@ const quadraticEquation = (e) => {
     );
     console.log(
       `	X = ${(b * -1 - d) / (a * 2)} or X = ${(b * -1 + d) / (a * 2)}`
+    );
+  } else {
+    console.log(
+      `the Discriminant is nigative: b^2 − 4ac = ${b}^2 - 4 * ${a} * ${c}`
+    );
+    console.log(`Discriminant = ${d}`);
+    console.log(`Put in a, b and c:	X = (${b * -1} ± √|${d}|) / 2 * ${a}`);
+    console.log(`So X = (${b * -1} ± ${Math.sqrt(d * -1)}i) / ${2 * a}`);
+    console.log("Where 'i' is the imaginary number √−1 ");
+    console.log(
+      `So X = ${(b * -1) / (2 * a)} ± ${Math.sqrt(d * -1) / (2 * a)}i `
     );
   }
 };
